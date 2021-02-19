de4972eaa5f478b3a8a9d8a4e1b40c3a951bb98b
Chaitanya Estarla <35031758+ChaitanyaEstarla@users.noreply.github.com>
2021-02-19 10:11:34 +0530

    Day 4
    Working on Pacman physics movment

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    private int rows = 20;
+    private int columns = 20;
+    private float tileCount = 1;
+    
+    void Start()
+    {
+        CreateBase();
+        CreateRoad();
+        CreatePellets();
+    }
+
+    private void CreateBase()
+    {
+        GameObject referenceTile = (GameObject) Instantiate(Resources.Load("BlackBG"));
+        
+        for (int row = -10; row < 10; row++)
+        {
+            for (int cols = -5; cols < 15; cols++)
+            {
+                GameObject tile = (GameObject) Instantiate(referenceTile, transform);
+
+                float posX = row;
+                float posY = cols;
+                
+                tile.transform.position = new Vector2(posX, posY);
+            }
+        }
+        Destroy(referenceTile);
+    }
+
+    private void CreateRoad()
+    {
+        GameObject referenceTile = (GameObject) Instantiate(Resources.Load("WhiteBox"));
+        
+        for (int row = -10; row < 10; row++)
+        {
+            for (int cols = -5; cols < 15; cols++)
+            {
+                GameObject tile = (GameObject) Instantiate(referenceTile, transform);
+
+                float posX = row;
+                float posY = cols;
+                
+                tile.transform.position = new Vector2(posX, posY);
+            }
+        }
+        Destroy(referenceTile);
+    }
+    
+    private void CreatePellets()
+    {
+        GameObject referenceTile = (GameObject) Instantiate(Resources.Load("Pellets"));
+        
+        for (int row = -10; row < 10; row++)
+        {
+            for (int cols = -5; cols < 15; cols++)
+            {
+                GameObject tile = (GameObject) Instantiate(referenceTile, transform);
+
+                float posX = row;
+                float posY = cols;
+                
+                tile.transform.position = new Vector2(posX, posY);
+            }
+        }
+        Destroy(referenceTile);
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PacmanManager.cs
+++ Assets/Scripts/PacmanManager.cs
@@ -1,41 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class PacmanManager : MonoBehaviour
 {
     public float speed = 0f;
+    
     private bool movePlayer = false;
     private int direction = 0;
+    private int angle = 0;
+    private Rigidbody2D pacman;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        pacman = gameObject.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             movePlayer = true;
             direction = 1;
+            angle = 180;
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             movePlayer = true;
             direction = 2;
+            angle = 0;
         }
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             movePlayer = true;
             direction = 3;
+            angle = 90;
         }
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             movePlayer = true;
             direction = 4;
+            angle = -90;
         }
         
         if (Input.GetKey(KeyCode.Space))
@@ -43,9 +50,19 @@ public class PacmanManager : MonoBehaviour
             movePlayer = false;
             direction = 0;
         }
-        if(movePlayer == true)
+    }
+
+    void FixedUpdate()
+    {
+        if(movePlayer)
         {
             MoveInDirection();
+            gameObject.GetComponent<Animator>().enabled = true;
+        }
+        
+        if (!movePlayer)
+        {
+            gameObject.GetComponent<Animator>().enabled = false;
         }
     }
     
@@ -54,17 +71,26 @@ public class PacmanManager : MonoBehaviour
         switch (direction)
         {
             case 1:
-                gameObject.transform.position += Vector3.left * Time.deltaTime * speed;
+                pacman.MovePosition(Vector2.right * Time.deltaTime * speed);
+                gameObject.transform.eulerAngles = Vector3.forward * angle;
                 break;
             case 2:
-                gameObject.transform.position += Vector3.right * Time.deltaTime * speed;
+                pacman.MovePosition(Vector2.left * Time.deltaTime * speed);
+                gameObject.transform.eulerAngles = Vector3.forward * angle;
                 break;
             case 3:
                 gameObject.transform.position += Vector3.up * Time.deltaTime * speed;
+                gameObject.transform.eulerAngles = Vector3.forward * angle;
                 break;
             case 4:
                 gameObject.transform.position += Vector3.down * Time.deltaTime * speed;
+                gameObject.transform.eulerAngles = Vector3.forward * angle;
                 break;
         }
+    }
+
+    public void StopPlayer()
+    {
+        movePlayer = false;
     }
 }

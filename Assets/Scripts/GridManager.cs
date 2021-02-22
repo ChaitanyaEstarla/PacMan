0e7972d59bec043fc262caa679d70d6d093e7511
Chaitanya Estarla <35031758+ChaitanyaEstarla@users.noreply.github.com>
2021-02-22 17:34:32 +0530

    Evening Backup
    Completed creating random grid for placing the path

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -4,53 +4,46 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    private bool decideTiles = true;
+    const int width = 20;
+    const int height = 10;
+
+    bool[,] referenceGrid = new bool[height, width];
     
     void Start()
     {
-        //CreateBase();
-        CreateRoad();
-        //CreatePellets();
+        ReferenceGrid();
+        CreateLevel();
     }
 
-    private void CreateBase()
-    { 
-        GameObject referenceTile = (GameObject) Instantiate(Resources.Load("4 Square"));
-        
-        for (int row = -10; row < 10; row++)
-        {
-            for (int cols = -5; cols < 15; cols++)
-            {
-                InstantiateTile(row, cols, referenceTile);
-            }
-        }
-        Destroy(referenceTile);
-    }
-
-    private void CreateRoad()
+    private void CreateLevel()
     {
         bool prevBuild = false;
-        GameObject referenceTile = (GameObject) Instantiate(Resources.Load("WhiteBox"));
         
-        for (int cols = 0; cols < 20; cols++)
+        GameObject roadTile = (GameObject) Instantiate(Resources.Load("WhiteBox"));
+        GameObject wallTile = (GameObject) Instantiate(Resources.Load("BlackBG"));
+        GameObject pellets = (GameObject) Instantiate(Resources.Load("Pellets"));
+
+        for (int cols = 0, i = 0; cols < height; cols++, i++)
         {
-            
-            for (int row = -10; row < 10; row++)
+
+            for (int row = -10, j = 0; row < height; row++, j++)
             {
-                if (decideTiles && !prevBuild)
+                if (referenceGrid[i,j])
                 {
-                    InstantiateTile(row, cols, referenceTile);
-                    
+                    InstantiateTile(row, cols, roadTile);
+                    InstantiateTile(row, cols, pellets);
                 }
-                else if(!decideTiles)
+
+                if (!referenceGrid[i, j])
                 {
-                    
+                    InstantiateTile(row, cols, wallTile);
                 }
             }
-            prevBuild = decideTiles;
-            decideTiles  = (Random.value > 0.5f);
         }
-        Destroy(referenceTile);
+
+        Destroy(roadTile);
+        Destroy(wallTile);
+        Destroy(pellets);
     }
 
     private void InstantiateTile(int rows, int columns, GameObject referenceTile)
@@ -63,17 +56,60 @@ public class GridManager : MonoBehaviour
         tile.transform.position = new Vector2(posX, posY);
     }
 
-    private void CreatePellets()
+    private void ReferenceGrid()
     {
-        GameObject referenceTile = (GameObject) Instantiate(Resources.Load("Pellets"));
+        bool road = true;
+        //int limit = 0;
+        bool prevColumn = false;
         
-        for (int row = -10; row < 10; row++)
+        for (int i = 0; i < height; i++)
         {
-            for (int cols = -5; cols < 15; cols++)
+            if (road)
             {
-                InstantiateTile(row, cols, referenceTile);
+                for (int j = 0; j < width; j++)
+                {
+                    referenceGrid[i, j] = road;
+                }
+            }
+            else
+            {
+                if (!prevColumn)
+                {
+                    for (int j = 0; j < width; j++)
+                    {
+                        referenceGrid[i, j] = referenceGrid[i-1, j];
+                    }
+                }
+                else
+                {
+                    int j = 0;
+                    while (j < width)
+                    {
+                        referenceGrid[i, j] = (Random.value > 0.5f);
+                        if (j == 0)
+                        {
+                            j++;
+                            continue;
+                        }
+                        if (referenceGrid[i, j - 1] && referenceGrid[i, j])
+                        {
+                            referenceGrid[i, j] = false;
+                        }
+                        j++;
+                    }
+                }
+            }
+
+            if (road)
+            {
+                prevColumn = road;
+                road = false;
+            }
+            else
+            {
+                prevColumn = road;
+                road = Random.value > 0.5f;
             }
         }
-        Destroy(referenceTile);
     }
 }
